21b757dd331a64480a1a2cdb104b46c2be63b34d
Femi <femzy14@gmail.com>
2024-05-13 18:48:16 +0100

    added features

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-export const apiUrl = 'http://localhost:5000/api/'
-// export const apiUrl = 'https://us-central1-chat-app-a13e6.cloudfunctions.net/api/api'
+// export const apiUrl = 'http://localhost:5000/api/'
+export const apiUrl = 'https://us-central1-chat-app-a13e6.cloudfunctions.net/api/api'
 export const host = 'http://localhost:5000'
+// export const host = 'https://proweb-one.vercel.app'
 
 let gg = ""
 
