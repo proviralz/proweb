8e37464ed60977b78f0992964a66eac860861726
Femi <femzy14@gmail.com>
2024-08-28 22:46:41 +0100

    added admin page

--- src/components/admin/dashboard/nav/Top.js
+++ src/components/admin/dashboard/nav/Top.js
@@ -1,9 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import { IoIosNotificationsOutline } from 'react-icons/io'
+import { RiAdminFill } from 'react-icons/ri'
+import { useSelector } from 'react-redux'
 
 const Top = ({isSelected}) => {
-  return (
+
+  const user = useSelector(state => state.admin.info)
+
+  if(!user) {
+    return;
+  }
+
+  return ( user &&
     <div>
       <div className=' py-5 px-10 flex items-center justify-between shadow-md bg-white'>
         <div>
@@ -13,23 +22,27 @@ const Top = ({isSelected}) => {
                 width={100} 
                 height={100} 
                 className=' w-full h-auto' />
+            
         </div>
         <div>
-            <p className=' capitalize'>
+            <p className=' capitalize font-semibold text-neutral-800'>
               {isSelected}
             </p>
         </div>
         <div className=' flex items-center gap-6'>
             <div>
-                <IoIosNotificationsOutline size={30} color='red' />
+                {/* <IoIosNotificationsOutline size={30} color='red' /> */}
             </div>
             <div className=' flex items-center gap-4'>
                 <div>
-                    <Image src={'/assets/profile/user1.svg'} alt=' user' width={100} height={100} className=' w-10 h-auto' />
+                    {/* <Image src={'/assets/profile/user1.svg'} alt=' user' width={100} height={100} className=' w-10 h-auto' /> */}
+                    <p>
+                      <RiAdminFill size={35} />
+                    </p>
                 </div>
                 <div>
                     <p className=' font-semibold'> 
-                        James Adams
+                        {user?.fullName}
                     </p>
                 </div>
             </div>
